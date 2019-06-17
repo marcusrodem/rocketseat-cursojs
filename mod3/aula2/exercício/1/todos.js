7243078f4e38580679a6ea620e679c197d520d20
unknown <marcus_rode@saojose.sc.senai.br>
2019-06-17 12:26:48 -0300

    terminamos a aula 2

--- mod3/aula2/exercício/1/todos.js
+++ mod3/aula2/exercício/1/todos.js
@@ -1,1 +1,6 @@
-alert("esta funcionando")
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+console.log(listElement);
+console.log(inputElement);
+console.log(buttonElement);
